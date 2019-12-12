df8e3058464917b669e23be91527743c82d8c64a
Sebastien Renault <srenault.contact@gmail.com>
2019-12-12 10:02:34 +0100

    Add cors headers for error response

--- src/main/scala/sre/api/CorsMidfleware.scala
+++ src/main/scala/sre/api/CorsMidfleware.scala
@@ -8,10 +8,7 @@ object CorsMiddleware {
   def addCorsHeader[F[_]: Effect](resp: Response[F]) = {
     val accessControlOrigin = Header("Access-Control-Allow-Origin", "*")
     val accessControlAllowMethods = Header("Access-Control-Allow-Methods", "GET")
-    resp match {
-      case Status.Successful(resp) => resp.putHeaders(accessControlOrigin, accessControlAllowMethods)
-      case resp => resp
-    }
+    resp.putHeaders(accessControlOrigin, accessControlAllowMethods)
   }
 
   def apply[F[_]: Effect](service: HttpService[F]) =
